58d6257e6d7b2776147dc8d0296e68de14a7f506
Rotom <sed4906birdie@gmail.com>
2022-07-24 07:19:38 -0700

    I tried...

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 A text editor written in Rust using Vulkan and imgui, because that makes total sense.
 ## Runtime Dependencies
 On Windows, requires the Visual C/C++ and Vulkan Runtimes.
+
 On Linux, requires Vulkan and GTK.
 ## Instructions
-Window->New: creates a window to edit a single file in.
+Window->New editor: creates a window to edit a single file in.
+
+Window->New shell (unimplemented): Creates a window to run commands in. Uses pwsh by default.
 
 Window->Close \[\#\]: closes the window with the given number.
 
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use simple_logger::SimpleLogger;
 use std::error::Error;
 use rfd::FileDialog;
 use std::fs;
+use std::process::Command;
+use std::process::Stdio;
+use std::process::Child;
+use std::io::Read;
 
 const APP_NAME: &str = "Parenthesis";
 
@@ -14,7 +18,9 @@ struct WindowData {
     text: String,
     path: String,
     pos: [f32; 2],
-    size: [f32; 2]
+    size: [f32; 2],
+    form: String,
+    cmd: Option<Child>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -23,8 +29,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     System::new(APP_NAME)?.run((), move |_, ui, _| {
         if let Some(menu_bar) = ui.begin_main_menu_bar() {
             if let Some(menu) = ui.begin_menu("Window") {
-                if MenuItem::new("New").build(ui) {
-                    windows.push(WindowData {text:"".into(), path:"".into(), size:[300.0,256.0], pos:[60.0,60.0]});
+                if MenuItem::new("New editor").build(ui) {
+                    windows.push(WindowData {text:"".into(), path:"".into(), size:[300.0,256.0], pos:[60.0,60.0],form:"editor".into(),cmd:None});
+                }
+                if MenuItem::new("New shell (unimplemented)").build(ui) {
+                    windows.push(WindowData {text:"".into(), path:"".into(), size:[300.0,256.0], pos:[60.0,60.0],form:"shell".into(),cmd:/*Some(
+                        Command::new("pwsh").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn().expect("could not spawn")
+                    )*/None});
                 }
                 for windowindex in 0..windows.len() {
                     let closename=format!("Close [{}]",windowindex+1);
@@ -48,46 +59,57 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let mut windownum = 1;
         for window in &mut windows {
-            let title=format!("[{}] {}",windownum, window.path);
+            let title=format!("[{}] {} ({})",windownum, window.path, window.form);
             Window::new(title)
                 .size(window.size, Condition::FirstUseEver)
                 .position(window.pos, Condition::FirstUseEver)
                 .menu_bar(true)
                 .build(ui, || {
-                    if let Some(menu_bar) = ui.begin_menu_bar() {
-                        if let Some(menu) = ui.begin_menu("File") {
-                            if MenuItem::new("New").build(ui) {
-                                window.text = "".into();
-                                window.path = "".into();
-                            }
-                            if MenuItem::new("Open").build(ui) {
-                                let files = FileDialog::new().pick_file();
-                                if let Some(ref pathbuf) = files {
-                                    window.text = fs::read_to_string(files.as_ref().unwrap()).unwrap();
-                                    window.path = pathbuf.to_str().unwrap().into();
+                    if window.form == "editor" {
+                        if let Some(menu_bar) = ui.begin_menu_bar() {
+                            if let Some(menu) = ui.begin_menu("File") {
+                                if MenuItem::new("New").build(ui) {
+                                    window.text = "".into();
+                                    window.path = "".into();
                                 }
-                            }
-                            if MenuItem::new("Save As").build(ui) {
-                                let res = rfd::FileDialog::new().save_file();
-                                if let Some(pathbuf) = res {
-                                    window.path = pathbuf.to_str().unwrap().into();
-                                    fs::write(&window.path,&window.text).expect("Failed to write to file");
+                                if MenuItem::new("Open").build(ui) {
+                                    let files = FileDialog::new().pick_file();
+                                    if let Some(ref pathbuf) = files {
+                                        window.text = fs::read_to_string(files.as_ref().unwrap()).unwrap();
+                                        window.path = pathbuf.to_str().unwrap().into();
+                                    }
                                 }
-                            }
-                            if MenuItem::new("Save").build(ui) {
-                                if !window.path.is_empty() {
-                                    fs::write(&window.path,&window.text).expect("Failed to write to file");
-                                } else {
+                                if MenuItem::new("Save As").build(ui) {
                                     let res = rfd::FileDialog::new().save_file();
                                     if let Some(pathbuf) = res {
                                         window.path = pathbuf.to_str().unwrap().into();
                                         fs::write(&window.path,&window.text).expect("Failed to write to file");
                                     }
                                 }
+                                if MenuItem::new("Save").build(ui) {
+                                    if !window.path.is_empty() {
+                                        fs::write(&window.path,&window.text).expect("Failed to write to file");
+                                    } else {
+                                        let res = rfd::FileDialog::new().save_file();
+                                        if let Some(pathbuf) = res {
+                                            window.path = pathbuf.to_str().unwrap().into();
+                                            fs::write(&window.path,&window.text).expect("Failed to write to file");
+                                        }
+                                    }
+                                }
+                                menu.end();
                             }
-                            menu.end();
+                            menu_bar.end();
                         }
-                        menu_bar.end();
+                    }
+                    if window.form == "shell" {
+                        window.text = "Shells are currently unimplemented.\nHelp with this would be appreciated.\n".into();
+                        /*let mut buf: [u8;1] = [0;1];
+                        let mut handle = window.cmd.as_mut().unwrap().stdout.as_mut().unwrap();
+                        match handle.read(&mut buf) {
+                            Ok(0) => {},
+                            _ => window.text.push_str(&String::from_utf8_lossy(&buf)),
+                        }*/
                     }
                     window.size = ui.window_size();
                     window.pos = ui.window_pos();
